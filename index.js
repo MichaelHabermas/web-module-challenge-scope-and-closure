42ca4606c7cbc42eb2087ad488bb271e0c44e6fc
MichaelHabermas <michaelghabermas@gmail.com>
2021-03-22 12:18:41 -0700

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -2,24 +2,27 @@
 
 /**Example Task : processFirstItem()
  * This example shows how you might go about solving the rest of the tasks
- * 
+ *
  * Use the higher order function processFirstItem below to do the following:
  *  1. Receive an array of strings in a parameter
  *  2. Receive a callback function that takes a string as its argument in a parameter
- *  3. Return the result of invoking the callback function and passing in the FIRST 
+ *  3. Return the result of invoking the callback function and passing in the FIRST
  *     element in the array as the argument
- * 
+ *
  * The following code is demonstrating a way of completing this task
  * It returns the string `foofoo`
-*/
+ */
 
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+	return callback(stringList[0]);
 }
-console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
+console.log(
+	processFirstItem(['foo', 'bar'], function (str) {
+		return str + str;
+	})
+);
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
@@ -28,19 +31,24 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  Counter 2 will continuously reference its global variable (as can any other function operation). 
+  counter1 creates a piece of memory and count that is only available to itself 
+
   2. Which of the two uses a closure? How can you tell?
-  
+  counter1. references another function that contains a function
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     The second is good if you want to coordinate 1 counter with many different processes. The first allows each to have its own counter
+
 */
 
 // counter1 code
 function counterMaker() {
-  let count = 0;
-  return function counter() {
-   return count++;
-  }
+	let count = 0;
+	return function counter() {
+		return count++;
+	};
 }
 
 const counter1 = counterMaker();
@@ -49,9 +57,8 @@ const counter1 = counterMaker();
 let count = 0;
 
 function counter2() {
-  return count++;
+	return count++;
 }
-
 
 /* ⚾️⚾️⚾️ Task 2: inning() ⚾️⚾️⚾️
 Use the inning function below to do the following:
@@ -62,10 +69,9 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning() {
+	return Math.floor(Math.random() * 3);
 }
-
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -79,10 +85,15 @@ Use the finalScore function below to do the following:
   "Home": 11,
   "Away": 5
 }
-*/ 
+*/
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning, numOfInnings) {
+	let score = { Home: 0, Away: 0 };
+	for (let i = 0; i < numOfInnings; i++) {
+		score.Home += inning();
+		score.Away += inning();
+	}
+	return score;
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,71 +101,87 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inning) {
+	let score = { Home: 0, Away: 0 };
+	score.Home += inning();
+	score.Away += inning();
+	return score;
 }
 
+// ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
+// Use the scoreboard function below to do the following:
+//   1. Receive the callback function `getInningScore` from Task 4
+//   2. Receive the callback function `inning` from Task 2
+//   3. Receive a number of innings to be played
+//   4. Return an array where each of it's index values equals a string stating the
+//   Home and Away team's scores for each inning.  Not the cummulative score.
+//   5. If there's a tie at the end of the innings, add this message containing the score to the end of the array:  "This game will require extra innings: Away 12 - Home 12"  (see tie example below)
+//      If there isn't a tie, add this message to the end of the array: "Final Score: Away 13 - Home 11"  (see no tie example below)
 
-/* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
-Use the scoreboard function below to do the following:
-  1. Receive the callback function `getInningScore` from Task 4
-  2. Receive the callback function `inning` from Task 2
-  3. Receive a number of innings to be played
-  4. Return an array where each of it's index values equals a string stating the
-  Home and Away team's scores for each inning.  Not the cummulative score.
-  5. If there's a tie at the end of the innings, add this message containing the score to the end of the array:  "This game will require extra innings: Away 12 - Home 12"  (see tie example below)
-     If there isn't a tie, add this message to the end of the array: "Final Score: Away 13 - Home 11"  (see no tie example below)
-  
-  NO TIE example: invoking scoreboard(getInningScore,inning, 9) might return 
-  an array of strings like this:
-[
-  "Inning 1: Away 1 - Home 2", 
-  "Inning 2: Away 2 - Home 1",
-  "Inning 3: Away 0 - Home 2", 
-  "Inning 4: Away 2 - Home 2", 
-  "Inning 5: Away 2 - Home 0", 
-  "Inning 6: Away 1 - Home 1", 
-  "Inning 7: Away 0 - Home 2", 
-  "Inning 8: Away 2 - Home 2",
-  "Inning 9: Away 1 - Home 0", 
-  "Final Score: Away 11 - Home 12"  
-]
+//  NO TIE example: invoking scoreboard(getInningScore,inning, 9) might return
+//   an array of strings like this:
+// [
+//   "Inning 1: Away 1 - Home 2",
+//   "Inning 2: Away 2 - Home 1",
+//   "Inning 3: Away 0 - Home 2",
+//   "Inning 4: Away 2 - Home 2",
+//   "Inning 5: Away 2 - Home 0",
+//   "Inning 6: Away 1 - Home 1",
+//   "Inning 7: Away 0 - Home 2",
+//   "Inning 8: Away 2 - Home 2",
+//   "Inning 9: Away 1 - Home 0",
+//   "Final Score: Away 11 - Home 12"
+// ]
 
-  TIE example: invoking scoreboard(getInningScore,inning, 9) might return 
-  an array of strings like this:
-[
-  "Inning 1: Away 1 - Home 1", 
-  "Inning 2: Away 2 - Home 2",
-  "Inning 3: Away 1 - Home 0", 
-  "Inning 4: Away 1 - Home 2", 
-  "Inning 5: Away 0 - Home 0", 
-  "Inning 6: Away 2 - Home 1", 
-  "Inning 7: Away 0 - Home 2", 
-  "Inning 8: Away 2 - Home 1",
-  "Inning 9: Away 1 - Home 1", 
-  "This game will require extra innings: Away 10 - Home 10"
-]  
-  */
+//   TIE example: invoking scoreboard(getInningScore,inning, 9) might return
+//   an array of strings like this:
+// [
+//   "Inning 1: Away 1 - Home 1",
+//   "Inning 2: Away 2 - Home 2",
+//   "Inning 3: Away 1 - Home 0",
+//   "Inning 4: Away 1 - Home 2",
+//   "Inning 5: Away 0 - Home 0",
+//   "Inning 6: Away 2 - Home 1",
+//   "Inning 7: Away 0 - Home 2",
+//   "Inning 8: Away 2 - Home 1",
+//   "Inning 9: Away 1 - Home 1",
+//   "This game will require extra innings: Away 10 - Home 10"
+// ]
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(getInningScore, inningerator, numOfInnings) {
+	const scoreboard = [];
+	let homeScore = 0;
+	let awayScore = 0;
+
+	for (let i = 0; i < numOfInnings; i++) {
+		let currentInning = getInningScore(inningerator);
+		scoreboard.push(`Inning ${i + 1}: Away ${currentInning.Away} - Home ${currentInning.Home}`);
+		homeScore += currentInning.Home;
+		awayScore += currentInning.Away;
+	}
+
+	if (homeScore !== awayScore) {
+		scoreboard.push(`Final Score: Away ${awayScore} - Home ${homeScore}`);
+	} else {
+		scoreboard.push(`This game will require extra innings: Away ${awayScore} - Home ${homeScore}`);
+	}
+	return scoreboard;
 }
 
-
-
+console.log(scoreboard(getInningScore, inning, 9));
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
-function foo(){
-  //console.log('its working');
-  return 'bar';
+function foo() {
+	//console.log('its working');
+	return 'bar';
 }
-export default{
-  foo,
-  processFirstItem,
-  counter1,
-  counter2,
-  inning,
-  finalScore,
-  getInningScore,
-  scoreboard,
-}
+export default {
+	foo,
+	processFirstItem,
+	counter1,
+	counter2,
+	inning,
+	finalScore,
+	getInningScore,
+	scoreboard
+};
